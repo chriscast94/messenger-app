befc8cd1c56348b8d4558d396192951e5df496c4
Christian Castillo <chriscast94@outlook.com>
2022-02-04 19:18:43 -0800

    Finished code

---
+++ README.md
@@ -0,0 +1,48 @@
+# Messenger-App
+
+## Table of Contents
+
+- [Description](#description)
+- [License](#license)
+- [Deployment](#deployment)
+- [Technology](#technology)
+- [Questions](#questions)
+
+## Description
+
+The application is a messaging up that you can look for friends to chat with by searching their name within the chat application. You can also create chat rooms within the app to collaborate, chat and send emojis with multiple people simultaneously.
+
+## License
+
+Copyright (c) 2022 Christian Castillo, Kenny Nguyen, Abraam Ibrahim
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Deployment
+
+## Technology
+
+bcrypt
+react-bootstrap
+
+apollo-client
+apollo-cache-inmemory
+apollo-link-ws
+bootstrap
+graphql
+package-json
+react-apollo
+react-bootstrapsubscriptions-transport-ws
+
+"react-bootstrap": "^2.1.2",
+"subscriptions-transport-ws": "^
+
+## Questions
+
+For any question, please contact us at
+
+GitHub: [Messenger-App](https://github.com/chriscast94/messenger-app)
--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { graphql, compose } from 'react-apollo';
+import { AuthService } from './util/Auth';
 
-import Signup from './pages/Signup';
+import User from './pages/Users';
+import SignUp from './pages/Signup';
 import Login from './pages/Login';
 import Message from './Message';
-import Nav from './components/navbar/Navbar'
+import Nav from './components/navbar/Navbar';
+
+import {
+    ADD_USER,
+    //UPDATE_USER,
+    DELETE_USER,
+    ADD_USER_SUB,
+    DELETE_USER_SUB
+} from './util/mutations';
+
+import { USER_QUERY } from '../src/util/queries';
+
 import './App.css';
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
@@ -22,29 +36,140 @@ const authLink = setContext((_, { headers }) => {
     };
 });
 
+const App = props => {
+    const ADD_USER = ADD_USER;
+    const user =
+        (localStorage.getItem('token') &&
+            JSON.parse(localStorage.getItem('token'))) ||
+        {};
 
+    const [receiverState, setReceiverState] = useState({
+        receiverMail: '',
+        receiverName: ''
+    });
 
+    const [userLeft, setUserLeft] = useState('');
 
+    const [hidden, setHidden] = useState(false);
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+    const setSelectedMail = (mail, user) => {
+        setReceiverState(receiverState => {
+            return { ...receiverState, receiverMail: mail, receiverName: user };
+        });
+        setHidden(!hidden);
+    };
 
-export default App;
+    const setStyle = () => {
+        setHidden(!hidden);
+    };
+
+    useEffect(() => {
+        const subscribeToMore = props.data.subscribeToMore;
+        subscribeToMore({
+            document: ADD_USER_SUB,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data)
+                    return prev;
+
+                const user = subscriptionData.data.newUser;
+
+                if (!prev.users.find(x => x.id === user.id)) {
+                    return { ...prev, users: [...prev.users, user] };
+                }
+                return prev;
+            }
+        });
+        subscribeToMore({
+            document: DELETE_USER_SUB,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data)
+                    return prev;
+
+                const oldUser = subscriptionData.data.oldUser;
+
+                if (prev.users.some(x => x.email === oldUser)) {
+
+                    const newUsers = prev.users.filter(x => x.email !== oldUser);
+
+                    prev.users = newUsers;
+
+                    return prev;
+                }
+                setUserLeft(oldUser);
+
+                return prev;
+            }
+        });
+    }, [props.data]);
+
+    const addUser = async (email, name) => {
+        await props.addUser({
+            variables: {
+                email,
+                name
+            },
+            update: (store, { data: { addUser } }) => {
+                const data = store.readQuery({ query: USER_QUERY });
+
+                if (!data.users.find(x => x.id === addUser.id)) {
+                    data.users.push(addUser);
+                }
+                store.writeQuery({ query: USER_QUERY, data });
+            }
+        });
+    };
+
+    const deleteUser = async email => {
+        localStorage.removeItem('token');
+        await props.deleteUser({
+            variables: { email },
+            update: store => {
+                const data = store.readQuery({ query: USER_QUERY });
+                data.users = data.users.filter(x => x.email !== email);
+                store.writeQuery({ query: USER_QUERY, data });
+            }
+        });
+    };
+
+    const { receiverMail, receiverName } = receiverState;
+
+    const {
+        data: { users, error, loading }
+    } = props;
+
+    if (loading || error) return null;
+    if (authLink.getItem('token')) {
+        return (
+            <div className="chat-page">
+
+                {/* <Switch> */}
+                <User
+                    style={{ display: hidden ? 'none' : 'block' }}
+                    users={users}
+                    email={user.email}
+                    name={user.name}
+                    selectedMail={setSelectedMail}
+                    deleteUser={deleteUser}
+                />
+                <Message
+                    style={{ display: hidden ? 'block' : 'none' }}
+                    email={user.email}
+                    receiverMail={receiverMail}
+                    receiverName={receiverName}
+                    userLeft={userLeft}
+                    name={user.name}
+                    setStyle={setStyle}
+                />
+                {/* </Switch> */}
+            </div >
+        );
+    }
+    //SignUp page
+    return <SignUp users={users} addUser={addUser} />;
+};
+
+export default compose(
+    graphql(USER_QUERY),
+    graphql(ADD_USER, { name: 'createUser' }),
+    graphql(DELETE_USER, { name: 'deleteUser' })
+)(App);
